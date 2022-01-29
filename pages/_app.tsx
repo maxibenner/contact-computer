0cca8f401e8b11a9f8ca3895e08cae89b40b5dc5
Maximilian Benner <maxibenner@gmail.com>
2022-01-29 22:37:14 +0100

    schema migration complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,43 +8,43 @@ import { AuthWrapper } from "../context/AuthContext";
 import { ProfileWrapper } from "../context/ProfileContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // return (
-  //   <div
-  //     style={{
-  //       display: "flex",
-  //       flexDirection: "column",
-  //       width: "500px",
-  //       maxWidth: "90vw",
-  //       color: "white",
-  //       position: "absolute",
-  //       top: "43%",
-  //       left: "50%",
-  //       transform: "translate(-50%, -50%)",
-  //     }}
-  //   >
-  //     <div style={{ fontSize: "4.8rem" }}>🚧</div>
-  //     <h1 style={{ margin: "10px 0", lineHeight: "5rem", fontSize: "4.8rem" }}>
-  //       Scheduled maintenance
-  //     </h1>
-  //     <p style={{ margin: 0, fontSize: "2.5rem" }}>
-  //       We will be back later today.
-  //     </p>
-  //   </div>
-  // );
   return (
-    <AuthWrapper>
-      <NotificationWrapper>
-        <ProfileWrapper>
-          <MouseWrapper>
-            <Wrapper>
-              <Component {...pageProps} />
-              {/* <_dev_tools_ /> */}
-            </Wrapper>
-          </MouseWrapper>
-        </ProfileWrapper>
-      </NotificationWrapper>
-    </AuthWrapper>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "500px",
+        maxWidth: "90vw",
+        color: "white",
+        position: "absolute",
+        top: "43%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <div style={{ fontSize: "4.8rem" }}>🚧</div>
+      <h1 style={{ margin: "10px 0", lineHeight: "5rem", fontSize: "4.8rem" }}>
+        Scheduled maintenance
+      </h1>
+      <p style={{ margin: 0, fontSize: "2.5rem" }}>
+        We will be back later today.
+      </p>
+    </div>
   );
+  // return (
+  //   <AuthWrapper>
+  //     <NotificationWrapper>
+  //       <ProfileWrapper>
+  //         <MouseWrapper>
+  //           <Wrapper>
+  //             <Component {...pageProps} />
+  //             {/* <_dev_tools_ /> */}
+  //           </Wrapper>
+  //         </MouseWrapper>
+  //       </ProfileWrapper>
+  //     </NotificationWrapper>
+  //   </AuthWrapper>
+  // );
 }
 
 export default MyApp;
